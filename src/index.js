1a00dc2c1fca611e92b9bf9a85e362ed8153938a
FlyaginAS <rus.reg.36@gmail.com>
2020-10-16 17:51:36 +0300

    6-7 создали компоненты заглушки

--- src/index.js
+++ src/index.js
@@ -1,41 +1,5 @@
-class SwapiService {
-  _apiBase = 'https://swapi.dev/api';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  getResource = async (url) => {
-    const res = await fetch(`${this._apiBase}${url}`);
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url} received ${res.status}`);
-    }
-    const body = await res.json();
-    return body;
-  };
-
-  getAllPeople = async () => {
-    const res = await this.getResource('/people/');
-    return res.results;
-  };
-  getPerson = async (id) => {
-    return this.getResource(`/people/${id}`);
-  };
-  getAllPlanets = async () => {
-    const res = await this.getResource('/planets/');
-    return res.results;
-  };
-  getPlanet = async (id) => {
-    return this.getResource(`/planets/${id}`);
-  };
-  getAllStarships = async () => {
-    const res = await this.getResource('/starships/');
-    return res.results;
-  };
-  getStarship = async (id) => {
-    return this.getResource(`/starships/${id}`);
-  };
-}
-
-const swapi = new SwapiService();
-swapi.getAllPeople().then((people) => {
-  people.forEach((item) => {
-    console.log(item.name);
-  });
-});
+ReactDOM.render(<App />, document.getElementById('root'));
