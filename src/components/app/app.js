6ea6ab045a888edca2883f239aedc2ab3af5c0b8
FlyaginAS <rus.reg.36@gmail.com>
2020-10-18 16:05:35 +0300

    7-9 component did catch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,11 +6,17 @@ import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
+import ErrorButton from '../error-button/error-button';
 
 export default class App extends Component {
   state = {
     selectedPerson: null,
+    hasError: false,
   };
+  componentDidCatch(err) {
+    console.log('componentDidCatch');
+    this.setState({ hasError: true });
+  }
 
   onPersonSelected = (id) => {
     this.setState({
@@ -19,11 +25,15 @@ export default class App extends Component {
   };
 
   render() {
+    if (this.state.hasError) {
+      return <span>OOps... the error</span>;
+    }
+
     return (
       <div>
         <Header />
         <RandomPlanet />
-
+        <ErrorButton />
         <div className="row mb2">
           <div className="col-md-6">
             <ItemList onPersonSelected={this.onPersonSelected} />
